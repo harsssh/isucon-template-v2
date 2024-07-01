976ac62e36fda41200ba9cc88f42ff6d5b097665
harsh <66548698+harsssh@users.noreply.github.com>
2024-07-02 04:20:18 +0900

    refactor

---
+++ assets/distribute_config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+
+config_dir_names=("common" "$(hostname)")
+
+for config_dir_name in "${config_dir_names[@]}"; do
+	(
+		cd "$config_dir_name" &&
+			find . -type f | while read -r rel_path; do
+				src_path=$(pwd)/$rel_path
+				dest_path=${rel_path#.}
+				cp -auv "$src_path" "$dest_path"
+			done
+	)
+done
