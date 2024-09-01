931e66e4fae59138f27aed1f674e06e0ad333cd2
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-09-01 11:43:48 +0900

    cd s1 失敗時のエラー出力を捨てる

--- assets/distribute_config.sh
+++ assets/distribute_config.sh
@@ -4,7 +4,7 @@ config_dir_names=("common" "$(hostname)")
 
 for config_dir_name in "${config_dir_names[@]}"; do
 	(
-		if cd "$config_dir_name"; then
+		if cd "$config_dir_name" &> /dev/null; then
 			find . -type f | while read -r rel_path; do
 				src_path=$(pwd)/$rel_path
 				dest_path=${rel_path#.}
